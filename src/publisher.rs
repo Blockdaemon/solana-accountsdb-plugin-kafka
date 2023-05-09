119f38d5e47e3979fc2c0ec072679e55fd26ab2a
Anthony Milbourne <18662115+amilbourne@users.noreply.github.com>
2023-05-09 23:41:35 +0100

    Added keys to slot and transaction messages
    Signed-off-by: Anthony Milbourne <18662115+amilbourne@users.noreply.github.com>

--- src/publisher.rs
+++ src/publisher.rs
@@ -51,14 +51,19 @@ impl Publisher {
     }
 
     pub fn update_slot_status(&self, ev: SlotStatusEvent) -> Result<(), KafkaError> {
+        let key = &ev.slot.to_le_bytes().to_vec();
         let buf = ev.encode_to_vec();
-        let record = BaseRecord::<(), _>::to(&self.slot_status_topic).payload(&buf);
+        let record = BaseRecord::<Vec<u8>, _>::to(&self.slot_status_topic)
+            .key(key)
+            .payload(&buf);
         self.producer.send(record).map(|_| ()).map_err(|(e, _)| e)
     }
 
     pub fn update_transaction(&self, ev: TransactionEvent) -> Result<(), KafkaError> {
         let buf = ev.encode_to_vec();
-        let record = BaseRecord::<(), _>::to(&self.transaction_topic).payload(&buf);
+        let record = BaseRecord::<Vec<u8>, _>::to(&self.transaction_topic)
+            .key(&ev.signature)
+            .payload(&buf);
         self.producer.send(record).map(|_| ()).map_err(|(e, _)| e)
     }
 
